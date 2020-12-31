2d55f01b5af95b716c73e0b6e56cf16f6e3f6b7f
韩初晨 <8237101+han-chuchen@user.noreply.gitee.com>
2020-12-31 16:26:50 +0800

    完成了分类功能的开发

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import './plugins/element.js'
 import './assets/css/global.css'
 //导入字体图标
 import './assets/fonts/iconfont.css'
-
+import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
 axios.interceptors.request.use(config => {
-    console.log(config)
+    // console.log(config)
     config.headers.Authorization = window.sessionStorage.getItem('token')
         // 在最后必须return config
     return config
@@ -21,6 +21,7 @@ axios.interceptors.request.use(config => {
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
+Vue.component('tree-table', TreeTable)
 
 new Vue({
     router,
